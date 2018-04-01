e13132f06233995faacdfc80c22ea9acf4c865e8
yotastalker <yotastalker@gmail.com>
2018-04-01 14:28:58 -0400

    Update_v1
    removed space after @ symbol.

--- Arduino sketches/MicroView-chris.mckearn_outlook.com/MicroView-chris.mckearn_outlook.com.ino
+++ Arduino sketches/MicroView-chris.mckearn_outlook.com/MicroView-chris.mckearn_outlook.com.ino
@@ -7,7 +7,7 @@ void setup()
   uView.clear(PAGE);//clear the page
   uView.flipHorizontal(true);
   uView.flipVertical(true);
-  uView.print("FIND ME @ \nchris.\n mckearn@\n .outlook\n.com");
+  uView.print("FIND ME @\nchris.\n mckearn@\n .outlook\n.com");
   uView.display();
 
 }
